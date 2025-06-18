c9670bc626057bdbb9d14de9b3009fb72418a338
YOUSEF EL TRACH <youssef.eltrach@gmail.com>
2025-06-18 12:02:36 +0100

    add headerShown false to tab screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -7,25 +7,29 @@ export default function TabLayout() {
         name="index"
         options={{
           title: "Home",
+          headerShown: false,
         }}
       />
       <Tabs.Screen
         name="search"
         options={{
           title: "Search",
+          headerShown: false,
         }}
       />
       <Tabs.Screen
         name="saved"
         options={{
           title: "Saved",
+          headerShown: false,
         }}
       />
       <Tabs.Screen
-        name="profile"
-        options={{
-          title: "Profile",
-        }}
+      name="profile"
+      options={{
+        title: "Profile",
+        headerShown: false,
+      }}
       />
     </Tabs>
   );
