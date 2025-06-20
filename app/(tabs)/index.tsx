c42af8d00838a4dfc87a80a01b75973a63557e33
YOUSEF EL TRACH <youssef.eltrach@gmail.com>
2025-06-21 00:38:06 +0100

    add movie fetching functionality

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,28 +1,50 @@
-
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
-import {  View, Image, ScrollView } from "react-native";
+import { fetchMovies } from "@/services/api";
+import useFetch from "@/services/useFetch";
 import { useRouter } from "expo-router";
+import React from "react";
+import { ActivityIndicator, Image, ScrollView, Text, View } from "react-native";
 
 export default function Index() {
   const router = useRouter();
+  const {
+    data: movies,
+    loading: moviesLoading,
+    error: moviesError,
+  } = useFetch(() => fetchMovies({ query: "" }));
 
   return (
     <View className="flex-1 bg-primary ">
       <Image source={images.bg} className="absolute w-full  z-20" />
-      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} 
-      contentContainerStyle={{minHeight: "100%", paddingBottom: 10}} >
-         <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
+      <ScrollView
+        className="flex-1 px-5"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ minHeight: "100%", paddingBottom: 10 }}
+      >
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto" />
 
-         <View className="flex-1 mt-5">
-          <SearchBar
-           onPress={() => router.push("/search")}
-          placeholder="Search for a movie"
+        {moviesLoading ? (
+          <ActivityIndicator
+            size="large"
+            color="#0000ff"
+            className="mt-10 self-center"
           />
-         </View>
+        ) : moviesError ? (
+          <Text>Error : {moviesError?.message}</Text>
+        ) : (
+          <View className="flex-1 mt-5">
+            <SearchBar
+              onPress={() => router.push("/search")}
+              placeholder="Search for a movie"
+            />
+            <>
+            <Text className="text-white">Latest Movies</Text>
+            </>
+          </View>
+        )}
       </ScrollView>
-     
     </View>
   );
 }
--- services/api.ts
+++ services/api.ts
@@ -1,8 +1,25 @@
-export const IMDB_CONFIG = {
-    BASE_URL : "https://imdb236.p.rapidapi.com/api/imdb",
+export const TMDB_CONFIG = {
+    BASE_URL : `https://api.watchmode.com/v1/sources/?apiKey=${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
     API_KEY : process.env.EXPO_PUBLIC_MOVIE_API_KEY ,
-    Headers: {
+    headers: {
       accept: "application/json",
       authorization:` "Bearer" ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
     }
-}                                           
+}                                 
+//https://api.watchmode.com/v1/search/          
+
+export const fetchMovies = async ({query}:{query:string}) => {
+    const endpoint = query
+      ? `${TMDB_CONFIG.BASE_URL}/v1/search?query=${encodeURIComponent(query)}`     
+      : `${TMDB_CONFIG.BASE_URL}/v1/releases/`;
+    const res = await fetch (endpoint,{
+      method: "GET",
+      headers: TMDB_CONFIG.headers,
+    });
+      if (!res.ok) {
+        throw new Error(`Error fetching movies: ${res.statusText}`);
+      
+    }
+    const data = await res.json();
+    return data; 
+}
