ad2da4c1a39e17f82723a5e67cc9cfd8da969929
YOUSEF EL TRACH <youssef.eltrach@gmail.com>
2025-06-25 12:48:19 +0100

    ufetching movies

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,74 +1,3 @@
-// import SearchBar from "@/components/SearchBar";
-// import { icons } from "@/constants/icons";
-// import { fetchMovies } from "@/services/api";
-// import useFetch from "@/services/useFetch";
-// import { useRouter } from "expo-router";
-// import React from "react";
-// import {
-//   ActivityIndicator,
-//   FlatList,
-//   Image,
-//   ScrollView,
-//   Text,
-//   View,
-// } from "react-native";
-
-// export default function Index() {
-//   const router = useRouter();
-//   const {
-//     data: movies,
-//     loading: moviesLoading,
-//     error: moviesError,
-//   } = useFetch(() => fetchMovies({ query: "" }));
-
-//   return (
-//      <View className="flex-1 bg-primary">
-//       <ScrollView className="flex-1 px-5"
-//         showsVerticalScrollIndicator={false}
-//         contentContainerStyle={{
-//           minHeight: "100%",
-//           paddingBottom: 10,
-//         }}>
-//           <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"  />
-//           {moviesLoading? (
-//             <ActivityIndicator
-//               size="large"
-//               color="#0000ff"
-//               className="mt-10"
-//             />
-//           ) : moviesError ? (
-//             <Text className="text-red-500 text-center mt-10">
-//             Error:  {moviesError?.message}
-//             </Text>
-//           ) : (
-//             <View className="flex-1 mt-5">
-//               <SearchBar
-//               onPress={() => router.push("/search")}
-//               placeholder="Search for movies"
-//               />
-//               <>
-//               <Text className="text-lg text-white font-bold mt-5 mb-3">Latest movies</Text>
-//               <FlatList
-//                 data={movies}
-//                 renderItem={({ item }) => (
-//                   <Text className="text-white text-sm">{item.title}</Text>
-//                 )}
-//                 keyExtractor={(item) => item.id.toString()}
-//                 numColumns={3}
-//                />
-             
-//               </>
-              
-//             </View>
-//           )}
-//       </ScrollView>  
-//     </View>
-    
-//   );
-// }
-
-
-
 import SearchBar from "@/components/SearchBar";
 import { icons } from "@/constants/icons";
 import { images } from "@/constants/images";
@@ -80,6 +9,7 @@ import {
   ActivityIndicator,
   FlatList,
   Image,
+  ScrollView,
   Text,
   View
 } from "react-native";
@@ -94,41 +24,44 @@ export default function Index() {
 
   return (
      <View className="flex-1 bg-primary">
-          <Image source={images.bg} className="absolute w-full  z-0"  />
+          <Image source={images.bg} className="absolute w-full z-0 " resizeMode="cover" />
+
+          <ScrollView className="flex-1 px-5"
+            showsVerticalScrollIndicator={false}
+            contentContainerStyle={{ minHeight: "100%", paddingBottom: 10}}>   
+
           <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"  />
+
           {moviesLoading? (
             <ActivityIndicator
               size="large"
               color="#0000ff"
-              className="mt-10"
+              className="mt-10 self-center"
             />
           ) : moviesError ? (
-            <Text className="text-red-500 text-center mt-10">
-            Error:  {moviesError?.message}
-            </Text>
+            <Text className="text-white" >Error:  {moviesError?.message}</Text>
           ) : (
-            <FlatList
-                data={movies}
-                renderItem={({ item }) => (
-                  <Text className="text-white text-sm">{item.title}</Text>
-                )}
-                keyExtractor={(item) => item.id.toString()}
-                numColumns={3}
-                ListHeaderComponent={() => (
-                  <View className="flex-1 mt-5 px-5">
+        
+                  <View className="flex-1 mt-5">
                     <SearchBar
                     onPress={() => router.push("/search")}
                     placeholder="Search for movies"
                     />
+                    <>
                     <Text className="text-lg text-white font-bold mt-5 mb-3">Latest movies</Text>
+                     <FlatList
+                        data={movies}
+                        renderItem={({ item }) => (
+                          <Text className="text-white text-sm">{item.title}</Text>
+                        )}
+                        keyExtractor={(item) => item.id.toString()}
+                        numColumns={3}
+                      />
+                    </>
                   </View>
-                )}
-                contentContainerStyle={{
-                  paddingBottom: 10,
-                }}
-                showsVerticalScrollIndicator={false}
-               />
+          
           )}
+          </ScrollView>
     </View>
     
   );
--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -5,17 +5,19 @@ import { Image, TextInput, View } from 'react-native';
 interface Props {
     placeholder:string ;
     onPress?: () => void;
+    value?: string;
+    onChangeText?: (text: string) => void;
 }
 
-export default function SearchBar  ({ placeholder, onPress }: Props) {
+export default function SearchBar  ({ placeholder, onPress, value, onChangeText }: Props) {
   return (
     <View className="flex-row items-center  bg-dark-200 rounded-full px-5 py-4">
       <Image source={icons.search} className="size-5" resize-contain tintColor="#ab8bff"  />
         <TextInput 
         onPress={onPress}
         placeholder={placeholder}
-        value=''
-        onChangeText={() => {}}
+        value={value}
+        onChangeText={onChangeText}
         placeholderTextColor="#a8b5db"
         className='flex-1 ml-2 text-white'
        />
